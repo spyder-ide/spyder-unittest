fe50ea0dd1ceb51fdff455484cb5d2d32c94b076
Gonzalo Peña-Castellanos <goanpeca@gmail.com>
2016-10-30 23:13:08 -0400

    Add version information and header

--- spyder_unittest/__init__.py
+++ spyder_unittest/__init__.py
@@ -1,1 +1,14 @@
+# -*- coding:utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright (c) Spyder Project Developers
+#
+# Licensed under the terms of the MIT License
+# (see spyder/__init__.py for details)
+# -----------------------------------------------------------------------------
+"""Spyder unitest plugin."""
+
+# Local imports
 from .unittest import UnitTestPlugin as PLUGIN_CLASS
+
+VERSION_INFO = (0, 1, 0, 'dev0')
+__version__ = '.'.join(map(str, VERSION_INFO))
