d5958f2132489dfbbc3d4cbd6ae84e7ece8d83a3
Jitse Niesen <jitseniesen@yahoo.com>
2017-01-20 16:30:52 +0000

    setup.py: Add Python 3.6 from classifiers and remove 3.4/3.5
    Fixes #53

--- setup.py
+++ setup.py
@@ -68,8 +68,7 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Testing',
         'Topic :: Text Editors :: Integrated Development Environments (IDE)'])
