8fab7db7d17608ec06c0f507a046a6f1ef8bbfe5
Jitse Niesen <jitseniesen@yahoo.com>
2017-04-10 21:28:09 +0100

    Add tests for FrameworkRegistry

---
+++ spyder_unittest/backend/tests/test_frameworkregistry.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2017 Spyder Project Contributors
+# Licensed under the terms of the MIT License
+# (see LICENSE.txt for details)
+"""Tests for frameworkregistry.py"""
+
+# Third party imports
+import pytest
+
+# Local imports
+from spyder_unittest.backend.frameworkregistry import FrameworkRegistry
+
+
+class MockRunner:
+    def __init__(self, *args):
+        self.init_args = args
+
+
+def test_frameworkregistry_when_empty():
+    reg = FrameworkRegistry()
+    with pytest.raises(KeyError):
+        reg.create_runner('foo', None, 'temp.txt')
+
+def test_frameworkregistry_after_registering():
+    reg = FrameworkRegistry()
+    reg.register('foo', MockRunner)
+    runner = reg.create_runner('foo', None, 'temp.txt')
+    assert isinstance(runner, MockRunner)
+    assert runner.init_args == (None, 'temp.txt')
