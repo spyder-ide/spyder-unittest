e990a48ead828515d08a0880ce73a4a665916bf8
Jitse Niesen <jitseniesen@yahoo.com>
2017-10-08 14:06:09 +0100

    Implement reader corresponding to JSONStreamWriter

--- spyder_unittest/backend/jsonstream.py
+++ spyder_unittest/backend/jsonstream.py
@@ -3,7 +3,18 @@
 # Copyright © 2013 Spyder Project Contributors
 # Licensed under the terms of the MIT License
 # (see LICENSE.txt for details)
-"""Reader and writer for sending stream of python objects using JSON."""
+r"""
+Reader and writer for sending stream of python objects using JSON.
+
+These classes can be used to send Python objects (specifically, ints, floats,
+strings, bools, lists, dictionaries or None) over a text stream. Partially
+received objects are correctly handled.
+
+Since multiple JSON-encoded objects cannot simply concatenated (i.e., JSON is
+not a framed protocol), every object is sent over the text channel in the
+format "N \n s \n", where the string s is its JSON encoding and N is the length
+of s.
+"""
 
 # Standard library imports
 import json
@@ -40,3 +51,41 @@ class JSONStreamWriter:
         txt = json.dumps(obj)
         self.stream.write(str(len(txt)) + '\n')
         self.stream.write(txt + '\n')
+
+
+class JSONStreamReader:
+    """
+    Reader for sending stream of Python objects using JSON.
+
+    This class is used to receive a stream sent by JSONStreamWriter.
+
+    Attributes
+    ----------
+    buffer : str
+       Text encoding an object that has not been completely received yet.
+    """
+
+    def __init__(self):
+        """Constructor."""
+        self.buffer = ''
+
+    def consume(self, txt):
+        """
+        Decode given text and return list of objects encoded in it.
+
+        If only a part of the encoded text of an object is passed, then it is
+        stored and combined with the remainder in the next call.
+        """
+        index = 0
+        res = []
+        txt = self.buffer + txt
+        while index < len(txt):
+            end_of_line1 = txt.find('\n', index)
+            len_encoding = int(txt[index:end_of_line1])
+            if end_of_line1 + len_encoding + 2 > len(txt):  # 2 for two \n
+                break
+            encoding = txt[end_of_line1 + 1:end_of_line1 + len_encoding + 1]
+            res.append(json.loads(encoding))
+            index = end_of_line1 + len_encoding + 2
+        self.buffer = txt[index:]
+        return res
--- spyder_unittest/backend/tests/test_jsonstream.py
+++ spyder_unittest/backend/tests/test_jsonstream.py
@@ -9,18 +9,46 @@
 from io import StringIO
 
 # Local imports
-from spyder_unittest.backend.jsonstream import JSONStreamWriter
+from spyder_unittest.backend.jsonstream import (JSONStreamReader,
+                                                JSONStreamWriter)
 
 
 def test_jsonstreamwriter_with_list():
     stream = StringIO()
     writer = JSONStreamWriter(stream)
     writer.write([1, 2])
-    assert writer.stream.getvalue() == '6\n[1, 2]\n'
+    assert stream.getvalue() == '6\n[1, 2]\n'
 
 
 def test_jsonstreamwriter_with_unicode():
     stream = StringIO()
     writer = JSONStreamWriter(stream)
     writer.write('三')
-    assert writer.stream.getvalue() == '8\n"\\u4e09"\n'
+    assert stream.getvalue() == '8\n"\\u4e09"\n'
+
+
+def test_jsonstreamreader_with_list():
+    reader = JSONStreamReader()
+    assert reader.consume('6\n[1, 2]\n') == [[1, 2]]
+
+
+def test_jsonstreamreader_with_unicode():
+    reader = JSONStreamReader()
+    assert reader.consume('8\n"\\u4e09"\n') == ['三']
+
+
+def test_jsonstreamreader_with_partial_frames():
+    reader = JSONStreamReader()
+    txt = '1\n2\n' * 3
+    assert reader.consume(txt[:2]) == []
+    assert reader.consume(txt[2:-2]) == [2, 2]
+    assert reader.consume(txt[-2:]) == [2]
+
+
+def test_isonsteamreader_writer_integration():
+    stream = StringIO()
+    writer = JSONStreamWriter(stream)
+    reader = JSONStreamReader()
+    writer.write([1, 2])
+    writer.write({'a': 'b'})
+    assert reader.consume(stream.getvalue()) == [[1, 2], {'a': 'b'}]
