0b3de17161b7684dadf320addb23b46ae1868bce
Jitse Niesen <jitseniesen@yahoo.com>
2019-12-31 22:56:38 +0000

    CI: Use correct token for coveralls

--- continuous_integration/circle/coverage.sh
+++ continuous_integration/circle/coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export COVERALLS_REPO_TOKEN=Kr503QwklmJYKXYRXLywrtw8zbX7K8SKx
+export COVERALLS_REPO_TOKEN=c5Qt1n27dLFCAIESYVkuCmVpUU8doney1
 source $HOME/miniconda/etc/profile.d/conda.sh
 conda activate test
 
